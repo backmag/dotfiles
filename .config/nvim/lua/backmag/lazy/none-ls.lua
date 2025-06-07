4e82c8c597e19d6cf2d05af4d7fc16f0d016428c
Gustaf Backman <gustaf.backman@spp.se>
2025-06-07 10:53:02 +0200

    Add remap <leader>en for nvim config + formatting

--- .config/nvim/lua/backmag/lazy/none-ls.lua
+++ .config/nvim/lua/backmag/lazy/none-ls.lua
@@ -9,12 +9,27 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier.with({
-          filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue", "css", "scss", "less", "html", "json", "yaml", "markdown", "graphql" },
-          extra_args = { "--no-semi" }
+          filetypes = {
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "vue",
+            "css",
+            "scss",
+            "less",
+            "html",
+            "json",
+            "yaml",
+            "markdown",
+            "graphql",
+          },
+          extra_args = { "--no-semi" },
         }),
         null_ls.builtins.formatting.isort,
         null_ls.builtins.formatting.black,
         require("none-ls.diagnostics.eslint_d"),
+        require("none-ls.formatting.eslint_d"),
       },
     })
   end,
--- .config/nvim/lua/backmag/lazy/telescope.lua
+++ .config/nvim/lua/backmag/lazy/telescope.lua
@@ -1,46 +1,49 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
 
-        tag = "0.1.6",
+    tag = "0.1.6",
 
-        dependencies = {
-            "nvim-lua/plenary.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").setup({})
+
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ph", builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>en", function()
+        builtin.find_files({
+          cwd = vim.fn.stdpath("config"),
+        })
+      end, {})
+
+      vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+      vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+
+      vim.keymap.set("n", "<leader>pws", function()
+        local word = vim.fn.expand("<cword>")
+        builtin.grep_string({ search = word })
+      end)
+      vim.keymap.set("n", "<leader>pWs", function()
+        local word = vim.fn.expand("<cWORD>")
+        builtin.grep_string({ search = word })
+      end)
+      vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
         },
-        config = function()
-            require("telescope").setup({})
-
-            local builtin = require("telescope.builtin")
-            vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-            vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-            vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
-            -- Replaced this with 'live_grep', seems smoother
-            --vim.keymap.set('n', '<leader>ps', function()
-            --    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-            --end)
-
-            vim.keymap.set("n", "<leader>pws", function()
-                local word = vim.fn.expand("<cword>")
-                builtin.grep_string({ search = word })
-            end)
-            vim.keymap.set("n", "<leader>pWs", function()
-                local word = vim.fn.expand("<cWORD>")
-                builtin.grep_string({ search = word })
-            end)
-            vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-        end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-            require("telescope").load_extension("ui-select")
-        end,
-    },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
 }
