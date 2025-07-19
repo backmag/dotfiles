fa942b4aa1c268abcbcb498f8d01328c6f3204de
Gustaf Backman <gustaf.backman@spp.se>
2025-07-19 23:45:05 +0200

    Update markdownpreview to install with npm

--- .config/nvim/lua/backmag/lazy/markdown.lua
+++ .config/nvim/lua/backmag/lazy/markdown.lua
@@ -1,14 +1,15 @@
 return {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-        --vim.fn["mkdp#util#install"]()
-        vim.fn.system("cd " .. vim.fn.stdpath("data") .. "/lazy/markdown-preview.nvim && npm install")
-    end,
-    config = function()
-        vim.g.mkdp_open_to_the_world = 1
-        vim.g.mkdp_open_ip = '127.0.0.1'
-        vim.g.mkdp_port = '8888'
-    end,
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+  config = function()
+    vim.g.mkdp_open_to_the_world = 1
+    vim.g.mkdp_open_ip = '127.0.0.1'
+    vim.g.mkdp_port = '8888'
+    vim.g.mkdp_browser = 'firefox'
+  end,
 }
